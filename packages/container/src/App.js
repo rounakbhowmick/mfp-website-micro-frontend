197a182947a72bbd2d98dc4ee3b6214d295add49
Rounak Bhowmick <rounakchakda@gmail.com>
2024-08-19 20:40:22 +0530

    yml updated

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <>
-      <h1>Hi There Rounadkss!</h1>
+      <h1>Hi There Rounas!</h1>
       <hr />
       <MarketingApp />
     </>
